075a490fb30350f449277631f0c920e2efe2edd0
woob0410 <73090762+woob0410@users.noreply.github.com>
2020-11-30 12:15:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scrool Sample
+// jQuery v1.9以降
+
+$(function(){
+
+   $('a[href^="#"]').click(function() {
+
+      // 初期設定：移動時間(ms)と頭出し位置
+      var speed = 500;
+      var offset = -160;
+
+      // アンカーを取得
+      var anchor = $(this).attr("href");
+
+      // ターゲットの位置を取得
+      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+      var position = target.offset().top + offset;
+
+      // スクロール（アニメーション）
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+      return false;
+
+   });
+
+});
